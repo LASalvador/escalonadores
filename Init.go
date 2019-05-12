8f4c5557aa04d01b6b766e71b2bc5d3d6ce25d45
Lucas Almeida Salvador <lucas.salvador11358@gmail.com>
2019-05-12 18:48:03 -0300

    fix - adicionando struct de processos

--- Init.go
+++ Init.go
@@ -1,21 +1,49 @@
 package main
 
 import "fmt"
+import "strconv"
+
+
+type Processo struct {
+	nome string
+	burst int
+	tcheg int
+	prioridade int
+	quantum int
+	tespera int 
+	tretorno int 
+	tfim int 
+	tresp int 
+}
 
 func main(){
 	var algoritmo int
 	var modo string
+	processos := []*Processo{}
+
 	fmt.Printf("Escolha seu algoritmo:\n1.FCFS\n2.SJF\n3.SRTF\n4.Round Robin\n5.Multinível\n\ta.primeiro nível - RR\n\tb.segundo nível - FCFS\n")
 	fmt.Scanln(&algoritmo)
 
-	fmt.Printf("Inserção de processos:\n Use L para ler arquivos\n use M para manual\n")
+	fmt.Printf("Inserção de processos:\n 1.use M para manual\n 2.Use L para ler arquivos\n")
 	fmt.Scanln(&modo)
 
 	//Trabalhar o modo de leitura de dados
 	if modo == "M" {
 		var qtd int
-		fmt.Printf("Digite a quantidade de processos")
+		var burst int
+
+		fmt.Printf("Digite a quantidade de processos: \n")
 		fmt.Scanln(&qtd)
+		for i:= 1; i<=qtd; i++ {
+			fmt.Printf("Digite o burst do Processo %d:\n", i)
+			fmt.Scanln(&burst)
+			p := new(Processo)
+			p.nome = "P" + strconv.Itoa(i)
+			p.burst = burst
+
+			processos = append(processos, p)
+		}
+
 	} else if modo == "L" {
 		fmt.Printf("Modo leitura")
 	} else {
@@ -24,6 +52,9 @@ func main(){
 
 	if algoritmo == 1{
 		fmt.Printf("algoritmo FCFS")
+		p := processos[0]
+		fmt.Println(p.nome)
+		fmt.Println(p.burst)
 	} else if algoritmo == 2{
 		fmt.Printf("algoritmo SJF")
 	} else if algoritmo == 3{
@@ -36,4 +67,4 @@ func main(){
 		fmt.Printf("Escolha inválida!!\n")
 	}
 
-}
+}	
